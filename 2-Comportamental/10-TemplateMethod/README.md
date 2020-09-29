72120549ca1708b724c581fe1770d9e42e0ef4ce
rafael zanatta <rafael.zanatta@upperds.com>
2020-09-29 15:38:30 -0300

    add template method

---
+++ 2-Comportamental/10-TemplateMethod/README.md
@@ -0,0 +1,5 @@
+# Resumo
+- Adia as etapas exatas deum algoritmo para uma subclasse
+
+# Definição
+- Defina o esqueleto de um algoritmo em uma operação, adiando algumas etapas para subclasses. Template Method permite que as subclasses redefinam certas etapas de um algoritmo sem alterar a estrutura do algoritmo
---
+++ 2-Comportamental/10-TemplateMethod/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass abA = new ConcreteClassA();
+            abA.TemplateMethod();
+
+
+            AbstractClass abB = new ConcreteClassB();
+            abB.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
